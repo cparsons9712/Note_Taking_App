a887ebeb7d944203ff2f35c37bd892b0f8ac173b
christine parsons <clong9712@gmail.com>
2023-03-27 20:37:25 -0400

    Fucking KILLING it mangit add .

--- func-y-town.js
+++ func-y-town.js
@@ -1,26 +1,26 @@
 // This object will hold all of the data for the notes
 let notes = {
     "week1":{
-        "Crazy": {
-            "notes" :"This is topic 1 notes",
-            "code" : "this should be a block of code",
-            "Lecture": "This is where  lecture notes would go" }
+        "Fluorescent Adolescent": {
+            "notes" :"You used to get it in your fishnets <br> now you only get it in your nightdress <br> Discarded all the naughty nights for niceness <br>Landed in a very common crisis <br>Everything's in order in a black hole <br>Nothing seems as pretty as the past though <br>That Bloody Mary's lacking in Tabasco <br>Remember when you used to be a rascal?",
+            "code" : "Fluorescent Adolescent is a ballad about getting older and finding out that life isn’t as fun anymore. It describes a woman who dreams of her youth while her current life is unsatisfying and boring, with emphasis on her sex life.",
+            "lecture": "Written and performed by Artic Monkeys" }
         ,
-        "Fun": {
-            "notes" :"This is topic 2 notes",
-            "code" : "this should be a block of code",
-            "Lecture": "This is where  lecture notes would go" }
+        "Prescription Kid": {
+            "notes" :"I'm just a prescription kid, I'll deal with it <br>I-I hate myself and the place I'm in <br>Got to better, I lost myself <br>Now I sleep on the floor, I can't help myself <br>I-I only want her 'cause she looks so fit <br>The sex is so good, I don't want to be fat <br>Flavorize like Americans live, and I'll be your prescription kid <br>I'll be your prescription kid",
+            "code" : "Yeah, I wrote that song when there was a lot of school shootings happening in America. It’s a first-person account of someone that’s really fucked up and has access to medication that he shouldn’t be prescribed. And he’s almost treating this scenario like a Bonnie and Clyde situation where he’s saying, “Let’s get a gun, Bonnie. We don’t have to live.” It’s a story of someone taking action about how they feel and then someone being insightful about how they feel. So there’s the first half, is where it’s really, really aggressive, someone basically saying, “Let’s kill ourselves” to his girlfriend. “We don’t have to live; guns are legal in America, so let’s go and buy one.” She also was in the fucking head, and then and hurting other people in the process. And the second half is basically not doing that and explaining how they feel. So there’s two sides to it. You see school shooters on the news, and I think it’s disgusting, but it doesn’t really matter. At the end of the day, they are sick people. You can say that with a lot of other horrific crimes that have been committed, doesn’t mean you have to sympathize with them at all. But to say that they’re well people would be criminal because they’re not well people. No one does that if they’re well, they’re mentally well, so instead of just saying “Fuck school shooters; fuck the NRA” like a lot of other artists do. It’s like, “OK, well, why don’t I take that energy and write something a bit more interesting?” That’s what that song is about.",
+            "lecture": "Kid Brunswick" }
     },
     "week2" : {
         "Coding": {
             "notes" :"Notes for topic 3",
             "code" : "this should be a block of code for topic ONE",
-            "Lecture": "This is where  lecture notes would go for Topic ONE" }
+            "lecture": "This is where  lecture notes would go for Topic ONE" }
         ,
         "Time": {
             "notes" :"block of notes for topic 4",
             "code" : "this should be a block of code for topic two",
-            "Lecture": "This is where  lecture notes would go for topic 2" }
+            "lecture": "This is where  lecture notes would go for topic 2" }
     }
 }
 
@@ -49,7 +49,6 @@ weekDiv.addEventListener("click", (w) => {
         let nav = document.querySelector('#topicNav')
         nav.append(topic)
     }
-})
 
 //when a topic is selected the header should change to be the same as that topic.
 let topicDiv = document.querySelector("#topicNav")
@@ -57,9 +56,28 @@ let header = document.querySelector('#title')
 topicDiv.addEventListener("click", t => {
     let topic = t.target.innerText
     header.innerHTML = `<h1> ${topic} </h1>`
-    
+    // connect the content type navigation to the content under topic so that the button is connecting to the correct data to populate main body div
+    let mainCont = document.querySelector(".contentSelection");
+    mainCont.addEventListener("click", (c) => {
+        //this needs to grab the value from the notes key in week.topic.notes and set the innerhtml of mainContent to equal the value.
+        let mainDis = document.querySelector('#mainContent')
+        let key = c.target.id
+        let weekTopic = notes[week]
+        let topicContent = weekTopic[topic]
+        mainDis.innerHTML = topicContent[key]
+    })
+})
+
+
 
 })
+
+/****************************TODO ****************
+make the fields for title and content clear on any button click
+add funtionality to add new topics (headers, contents for notes, code, lecture)
+add functionality to take current saved content and place it into a input pield for edits and updates
+
+
 
 
 
